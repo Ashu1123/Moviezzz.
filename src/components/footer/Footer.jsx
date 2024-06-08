a5ceb52d51036c90e470b3229d69039c1e7274c5
Ashu1123 <114846825+Ashu1123@users.noreply.github.com>
2024-06-08 17:40:03 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Innovative Software Engineer with over 4 years of experience in
+          developing web-based solutions using HTML, CSS, JavaScript, and
+          React.js. Proficient in Redux Toolkit and dedicated to enhancing user
+          experiences by delivering high-quality solutions. Proven track record
+          in collaborating with cross-functional teams to deliver projects on
+          time and within budget.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
